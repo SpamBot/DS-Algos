85e62e5fe0aa1313dd3b3f49222f1d43e70fd519
Spam-B0t <56346455+Spam-B0t@users.noreply.github.com>
2019-12-09 17:52:53 +0200

    Create bin_to_dec.c

---
+++ C/Converters/bin_to_dec.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+#define abs(x) ((x)>=0) ? (x):-(x)
+#define print(x) printf("%d", x)
+
+
+int btod(char *s, int d){
+    int n=strlen(s);
+    if(n>0){
+        return d*((int)s[0]-48)+btod(s+1, 2*d);
+    }
+    else return 0;
+}
+
+void reverse(char *s){
+    int i;
+    char c;
+    for(i=0;i<strlen(s)/2;i++){
+        c=s[i];
+        s[i]=s[strlen(s)-1-i];
+        s[strlen(s)-1-i]=c;
+    }
+}
+
+int main(){
+    char s[100], *p;
+    gets(s);
+    if(s[0]=='-'){printf("-"); p=s+1;}
+    else p=&s;
+    reverse(p);
+    print(btod(p, 1));
+}
