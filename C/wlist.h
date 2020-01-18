3fddc7b1387c5875203397ae573a8b8a7f7e4df4
Spam-B0t <56346455+Spam-B0t@users.noreply.github.com>
2020-01-18 10:39:15 +0200

    Now list contains dynamical strings.
    More efficient memory use, increased speed.

--- C/wlist.h
+++ C/wlist.h
@@ -4,8 +4,8 @@
 
 
 typedef struct{
-    int num;
-    char val[50];//wsize=50
+    int num, msize;
+    char *val;
 }word;
 //word list
 typedef struct{
@@ -26,7 +26,7 @@ wlist initwlist(){
     return l;
 }
 
-int searchwl(wlist *li, char *s){//O(wsize*(log(size)+1))
+int searchwl(wlist *li, char *s){//O(max word msize*(log(size)+1))
     int l=0, r=li->size, m, tst, t=1;//t is <0 if we found string and >0 otherwise
     while(l<r-1){
         m=(l+r)/2;
@@ -35,7 +35,7 @@ int searchwl(wlist *li, char *s){//O(wsize*(log(size)+1))
         else if(tst>0)l=m;
         else {l=m; t=-1; break;}
     }
-    if(li->size>0 &&  strcmp(s, &li->data[l].val[0])>0)l=r;
+    if(li->size>0 && strcmp(s, &li->data[l].val[0])>0)l=r;
     return l*t;
 }
 //if i<0 we increase counter, if i>0 we add s to data[]
@@ -45,13 +45,16 @@ void insertwl(wlist *l, char *s, int i){//O(wsize+2*size)
         int j;
         l->size++;
         if(l->size==l->msize){
-            l->msize+=20;
+            l->msize+=50;
             l->data=(word *)realloc(l->data, sizeof(word)*l->msize);
         }
         for(j=l->size-1; j>i; j--)l->data[j]=l->data[j-1];
         l->data[i].num=1;
+        l->data[i].msize=strlen(s)+1;
+        l->data[i].val=(char *)calloc(l->data[i].msize, sizeof(char));
+        //printf("%s\n", s); printlist(l);
         strcpy(l->data[i].val, s);
-    }
+    } 
 }
 //bubble sort for sorting list by word frequency without changing the relative order
 void bsortli(wlist *li, int n){
@@ -70,7 +73,7 @@ void mergew(word *a, int n, int m){
     word out[m];
     while(i<n && j<m){
         if(a[i].num<a[j].num){out[t]=a[j]; t++; j++;}//this line causes "stability" property
-        else {out[t]=a[i]; t++; i++;}                //and this one too
+        else {out[t]=a[i]; t++; i++;}                //and this one too (устойчивость)
     }
     while(i<n){out[t]=a[i]; t++; i++;}
     while(j<m){out[t]=a[j]; t++; j++;}
