d4060ee893e37284030c2234267cb81c99fcc0fb
SpamBot <56346455+SpamBot@users.noreply.github.com>
2020-09-02 22:35:09 +0300

    +long multiplication, misc style updates

--- C/hyper.h
+++ C/hyper.h
@@ -4,41 +4,58 @@
 
 
 typedef struct{
-    int n, val[10000];
+    int len; long val[10000];
 }hyper;
 
 void inith(hyper *h){
-    h->n=1; h->val[0]=0;
+    h->len=1; h->val[0]=0;
 }
 
-void addh(hyper *a, hyper *b){
-    int n=min(a->n, b->n), r=0, x;
-    for(int i=0; i<n; i++){
-        x=a->val[i]+b->val[i]+r;
-        r=x/10000;
+void copyh(hyper *a, hyper *b){
+    for(int i=0; i<b->len; i++)a->val[i]=b->val[i];
+    a->len=b->len;
+}
+
+void addh(hyper *a, hyper *b, int shift){
+    int i=0;
+    long x, rem=0;
+    while(i<b->len || rem>0){
+        x=rem;
+        if(i+shift<a->len)x+=a->val[i+shift];
+        else while(i+shift>=a->len){a->val[a->len]=0; a->len++;}
+        if(i<b->len)x+=b->val[i];
+        a->val[i+shift]=x%10000;
+        rem=x/10000;
+        i++;
+    }
+}
+
+void mult_short(hyper *a, int b){
+    int i;
+    long x, rem=0;
+    for(i=0; i<a->len; i++){
+        x=a->val[i]*b+rem;
         a->val[i]=x%10000;
+        rem=x/10000;
     }
-    if(a->n<b->n)for(int i=n; i<b->n; i++){
-        x=b->val[i]+r;
-        r=x/10000;
-        a->val[i]=x%10000;
-        a->n++;
-    }
-    if(r>0){a->val[n]=r; a->n++;}
+    while(rem>0){a->len++; a->val[i]=rem%10000; rem/=10000; i++;}
 }
 
-void mult(hyper *h, int n){
-    long r=0, x;//6561*3 19683 9683
-    for(int i=0; i<h->n; i++){
-        x=h->val[i]*n+r; //printf("x=%ld\n", x);
-        r=x/10000;
-        h->val[i]=x%10000;
+void mult_hyper(hyper *a, hyper *b){
+    hyper a_tmp, tmp;
+    copy(&a_tmp, a);
+    copy(&tmp, &a_tmp);
+    mult_short(&tmp, b->val[0]);
+    copy(a, &tmp);
+    for(int i=1; i<b->len; i++){
+        copy(&tmp, &a_tmp);
+        lmults(&tmp, b->val[i]);
+        add(a, &tmp, i);
     }
-    if(r>0){h->val[h->n]=r; h->n++;}
 }
 
-void print(hyper *a){
-    printf("%d", a->val[a->n-1]);
-    for(int i=a->n-2; i>=0; i--)
+void print_hyper(hyper *a){
+    printf("%d", a->val[a->len-1]);
+    for(int i=a->len-2; i>=0; i--)
         printf("%.4d", a->val[i]);
 }
