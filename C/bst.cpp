1b50009a7dc946b58fb47b78488ee4cdd7ed89f2
SpamBot <56346455+SpamBot@users.noreply.github.com>
2021-11-17 19:03:31 +0200

    Create bst.cpp

---
+++ C/bst.cpp
@@ -0,0 +1,19 @@
+typedef struct{//aka binary indexed tree
+    std::vector<ll> val;
+    long size;
+    ll psum(long n){
+        ll out=0;
+        while(n>0){
+            out+=val[n];
+            n=(n & (n+1)) - 1;
+        }
+        return out;
+    }
+    ll rsq(long l, long r){return psum(r)-psum(l-1);}
+    void upd(long k, long d){
+        while(k<size){
+            val[k]+=d;
+            k=k | (k+1);
+        }
+    }
+}fenwick;
